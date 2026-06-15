c02ade6d59ec1fef33ff1660768214d1cc92606b
Coder-Utkarsh72 <utkarshchaurasiya7272@gmail.com>
2026-06-15 16:46:30 -0700

    updated Port

--- server.js
+++ server.js
@@ -20,10 +20,15 @@ app.use(
           -> App Passwords -> "Mail" select -> 16 digit password copy
 */
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
